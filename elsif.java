acccf4b8f862b95134798f7a346caac86eca6884
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-05-15 02:47:45 +0530

    Create elsif.java

---
+++ elsif.java
@@ -0,0 +1,15 @@
+package conditionalstatement;
+
+public class ConditionalStatement {
+
+public static void main(String[] args) {
+        int a=0;
+        if(a>0)
+            System.out.println("number is positive");
+        else if(a<0)
+            System.out.println("number is negative");
+        else
+            System.out.println("number is zero");
+    }
+    
+}
