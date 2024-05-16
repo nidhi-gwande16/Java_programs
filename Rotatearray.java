18deed2d48a1fb104ed06a462e54e95dd48b0cf0
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-05-16 23:37:28 +0530

    Rotate elements

---
+++ Rotatearray.java
@@ -0,0 +1,36 @@
+package arrays;
+import java.util.Arrays;
+
+public class RotateArray{
+    
+public static void main(String args[]){
+        int nums[]={1,2,4,5};
+        int k=6;
+        System.out.println(Arrays.toString(rotate(nums,k)));
+    }
+
+    
+    
+        public static int[] rotate(int[] nums, int k)
+        {
+            int n= nums.length;
+            k=k%n;
+            reverse(nums,0,n-1);
+            reverse(nums,0,k-1);
+            reverse(nums,k,n-1);
+            return nums;
+        }
+         public static void reverse(int[] nums, int start, int end)
+         {
+             while(start<=end)
+             {
+                 int temp=nums[start];
+                 nums[start]=nums[end];
+                 nums[end]=temp;
+                 start++;
+                 end--;
+             }
+         }
+
+        
+}
