9d8ee69f24bb7639c1bff06357df47d5567e9628
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-05-29 23:55:23 +0530

    isomorphic

---
+++ isomor.java
@@ -0,0 +1,37 @@
+class String {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length())  //length must be same for both strings
+        return false;
+
+        HashMap<Character,Character> map1=new HashMap<>();
+        HashMap<Character,Boolean> map2=new HashMap<>();
+
+        int n=s.length(); //As both string's length are same
+
+        for(int i=0;i<n;i++)
+        {
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+
+            if(map1.containsKey(ch1)==true)
+            {
+                if(map1.get(ch1)!=ch2)
+                return false;
+
+            }
+            else
+            {
+                if(map2.containsKey(ch2)==true)
+                return false;
+                else 
+                {
+                    map1.put(ch1,ch2);
+                    map2.put(ch2,true);
+
+                }
+            }
+
+        }
+        return true;
+    }
+}  
