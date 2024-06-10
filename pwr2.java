d4d6ed8012eb629756adc646b0a35028da40d8c5
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-06-10 21:52:02 +0530

    Create pwr2.java

---
+++ pwr2.java
@@ -0,0 +1,27 @@
+package power;
+
+public class PowerOfTwo {
+            public static void main(String args[]){
+                 int n=16;
+                 System.out.println(IsPowerOfTwo(n));
+            }
+            public static boolean IsPowerOfTwo(int n){
+                if(n==0)
+                {
+                    return false;
+                }
+                while(n!=1)
+                {
+                    if(n%2!=0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        n=n/2;
+                    }
+                }
+                 return true;
+            }
+    
+}
