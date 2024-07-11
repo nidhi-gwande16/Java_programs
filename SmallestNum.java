e08321e66d3145873c7d9d18e648698e28c2354f
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-07-11 23:43:18 +0530

    Create SmallestNum.java

---
+++ SmallestNum.java
@@ -0,0 +1,22 @@
+package functions;
+
+import java.util.Scanner;
+
+public class SmallestNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+        System.out.println("Enter third  number");
+        int c=sc.nextInt();
+        System.out.println("The smallest number among a,b,c is "+ smallest(a,b,c));
+        
+    }
+    
+    public static int smallest(int a,int b,int c)
+    {
+        return Math.min(Math.min(a,b),c);
+    } 
+}
